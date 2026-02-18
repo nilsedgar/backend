266430a92b44f1943401b49a60d88ae186db4110
nilsedgar <nils.jacobsen@zocom.se>
2026-02-18 11:38:51 +0100

    hej

--- server.js
+++ server.js
@@ -6,12 +6,20 @@ const cn = db.createConnection({
     host: "localhost",
     user: "root",
     password: "nisse",
-    database: "filmbibliotek"
+    database: "webshoppen"
 });
 
+app.use(express.json())
 
 app.get("/hello", (req, res) => {
-    cn.query("SELECT * FROM filmer", (err, data) => {res.send(data)});
+    cn.query("SELECT * FROM products", (err, data) => {res.send(data)});
+})
+
+app.post("/admin/create-product", (req, res) =>  {
+    cn.query("INSERT INTO products (name, price, stock_quantity, categories_id) VALUES (?, ?, ?, ?);",
+        [req.body.name, req.body.price, req.body.stock_quantity, req.body.categories_id]
+    )
+    res.send("Klart!")
 })
 
 app.listen(PORT);
