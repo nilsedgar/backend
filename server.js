dc3f8689790bd6bd9dab213b8ba56557a2bb6298
nilsedgar <nils.jacobsen@zocom.se>
2026-02-23 09:04:33 +0100

    stor

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.get("/hello", (req, res) => {
     cn.query("SELECT * FROM products", (err, data) => {res.send(data)});
 })
 
+
+
 app.post("/admin/create-product", (req, res) =>  {
     cn.query("INSERT INTO products (name, price, stock_quantity, categories_id) VALUES (?, ?, ?, ?);",
         [req.body.name, req.body.price, req.body.stock_quantity, req.body.categories_id]
